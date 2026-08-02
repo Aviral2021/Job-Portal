bcf67c9dfad19164e30b581aefb508241c116872
aviral <aviral.ece.ipu@gmail.com>
2026-08-02 11:26:55 +0530

    feat: add category carsousel, hero section and improve homepage UI

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarouse1 from './CategoryCarouse1'
 
 export const Home = () => {
   return (
     <div>Home
     <Navbar/>
+    <HeroSection/>
+
+
+    
+     <CategoryCarouse1/>
+    {/* <LatestJobs/> */}
+    {/* <Footer/>  */}
     </div>
   )
 }
