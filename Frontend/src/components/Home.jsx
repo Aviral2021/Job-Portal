54c675488c8b675e5c09c2fbb36a98637ee7a68a
aviral <aviral.ece.ipu@gmail.com>
2026-08-03 10:25:42 +0530

    feat: latejobscards and latesjobs

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CategoryCarouse1 from './CategoryCarouse1'
+import LatestJobs from './LatestJobs'
+import Footer from './Footer'
 
 export const Home = () => {
   return (
-    <div>Home
+    <div>
     <Navbar/>
     <HeroSection/>
-
-
-    
      <CategoryCarouse1/>
-    {/* <LatestJobs/> */}
-    {/* <Footer/>  */}
+    <LatestJobs/> 
+    <Footer/> 
     </div>
   )
 }
